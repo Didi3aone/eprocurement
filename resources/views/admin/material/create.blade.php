c9c2680de671af6a8c89a342f8ec19de589e918b
Elvis Sonatha <elviskudo@gmail.com>
2020-03-20 06:32:07 +0700

    bug fix multiple

--- resources/views/admin/material/create.blade.php
+++ resources/views/admin/material/create.blade.php
@@ -48,9 +48,9 @@
                             <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
                             <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
                         </div>
-                        <select class="form-control select2 {{ $errors->has('m_group_id') ? 'is-invalid' : '' }}" name="m_group_id[]" id="m_group_id" required>
+                        <select class="form-control select2 {{ $errors->has('m_group_id') ? 'is-invalid' : '' }}" name="m_group_id" id="m_group_id" required>
                             @foreach($materialGroups as $id => $mg)
-                                <option value="{{ $id }}" {{ in_array($id, old('m_group_id', [])) ? 'selected' : '' }}>{{ $mg->code }} - {{ $mg->description }}</option>
+                                <option value="{{ $id }}" {{ old('m_group_id', '') ? 'selected' : '' }}>{{ $mg->code }} - {{ $mg->description }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -60,9 +60,9 @@
                             <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
                             <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
                         </div>
-                        <select class="form-control select2 {{ $errors->has('m_type_id') ? 'is-invalid' : '' }}" name="m_type_id[]" id="m_type_id" required>
+                        <select class="form-control select2 {{ $errors->has('m_type_id') ? 'is-invalid' : '' }}" name="m_type_id" id="m_type_id" required>
                             @foreach($materialTypes as $id => $mt)
-                                <option value="{{ $id }}" {{ in_array($id, old('m_type_id', [])) ? 'selected' : '' }}>{{ $mt->code }} - {{ $mt->description }}</option>
+                                <option value="{{ $id }}" {{ old('m_type_id', '') ? 'selected' : '' }}>{{ $mt->code }} - {{ $mt->description }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -72,9 +72,9 @@
                             <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
                             <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
                         </div>
-                        <select class="form-control select2 {{ $errors->has('m_plant_id') ? 'is-invalid' : '' }}" name="m_plant_id[]" id="m_plant_id" required>
+                        <select class="form-control select2 {{ $errors->has('m_plant_id') ? 'is-invalid' : '' }}" name="m_plant_id" id="m_plant_id" required>
                             @foreach($plants as $id => $pl)
-                                <option value="{{ $id }}" {{ in_array($id, old('m_plant_id', [])) ? 'selected' : '' }}>{{ $pl->code }} - {{ $pl->description }}</option>
+                                <option value="{{ $id }}" {{ old('m_plant_id', '') ? 'selected' : '' }}>{{ $pl->code }} - {{ $pl->description }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -84,9 +84,9 @@
                             <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
                             <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
                         </div>
-                        <select class="form-control select2 {{ $errors->has('m_purchasing_id') ? 'is-invalid' : '' }}" name="m_purchasing_id[]" id="m_purchasing_id" required>
+                        <select class="form-control select2 {{ $errors->has('m_purchasing_id') ? 'is-invalid' : '' }}" name="m_purchasing_id" id="m_purchasing_id" required>
                             @foreach($purchasingGroups as $id => $pg)
-                                <option value="{{ $id }}" {{ in_array($id, old('m_purchasing_id', [])) ? 'selected' : '' }}>{{ $pg->code }} - {{ $pg->description }}</option>
+                                <option value="{{ $id }}" {{ old('m_purchasing_id', '') ? 'selected' : '' }}>{{ $pg->code }} - {{ $pg->description }}</option>
                             @endforeach
                         </select>
                     </div>
@@ -96,9 +96,9 @@
                             <span class="btn btn-info btn-xs select-all" style="border-radius: 0">{{ trans('global.select_all') }}</span>
                             <span class="btn btn-info btn-xs deselect-all" style="border-radius: 0">{{ trans('global.deselect_all') }}</span>
                         </div>
-                        <select class="form-control select2 {{ $errors->has('m_profit_id') ? 'is-invalid' : '' }}" name="m_profit_id[]" id="m_profit_id" required>
+                        <select class="form-control select2 {{ $errors->has('m_profit_id') ? 'is-invalid' : '' }}" name="m_profit_id" id="m_profit_id" required>
                             @foreach($profitCenters as $id => $pc)
-                                <option value="{{ $id }}" {{ in_array($id, old('m_profit_id', [])) ? 'selected' : '' }}>{{ $pc->code }} - {{ $pc->description }}</option>
+                                <option value="{{ $id }}" {{ old('m_profit_id', '') ? 'selected' : '' }}>{{ $pc->code }} - {{ $pc->description }}</option>
                             @endforeach
                         </select>
                     </div>
