3c9690eeebd4ff5bc565922cf139ca8c948dd24c
didi3aone <didithreeaone@gmail.com>
2020-07-15 20:32:52 +0700

    show ulang

--- app/Models/RequestNotesDetail.php
+++ app/Models/RequestNotesDetail.php
@@ -25,6 +25,13 @@ class RequestNotesDetail extends Model
         'updated_at'
     ];
 
+    public const PurchaseRequest = 1;
+    public const RequestNote     = 0;
+    public const GoodIssue       = 4;
+    public const Material        = 0;
+    public const NonMaterial     = 9;
+    public const MaterialText    = 99;
+
     public function getRnNo()
     {
     	return $this->hasOne(\App\Models\RequestNote::class, 'id', 'request_id');
