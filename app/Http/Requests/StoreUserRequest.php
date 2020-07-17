97407da9e2e7d4d4a145e5fa746d138358b1ccce
didi3aone <didithreeaone@gmail.com>
2020-07-17 17:29:58 +0700

    los email

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -24,7 +24,7 @@ class StoreUserRequest extends FormRequest
             ],
             'email'    => [
                 'required',
-                'unique:users',
+                // 'unique:users',
             ],
             'password' => [
                 'required',
