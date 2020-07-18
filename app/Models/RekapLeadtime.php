507d104377abfca9fca8b2b6c64e699f6335a27a
didi3aone <didithreeaone@gmail.com>
2020-07-18 09:04:06 +0700

    fix lead time

--- app/Models/RekapLeadtime.php
+++ app/Models/RekapLeadtime.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class RekapLeadtime extends Model
 {
     protected $connection = 'pgsql';
-    public static function getLeadTime($material, $plant, $pg)
-    {
-        return RekapLeadtime::where('material_id', $material)
-            ->where('plant_code', $plant)
-            ->where('purchasing_group', $pg)
-            ->first();
-    }
 
     public static function calculateLeadTime($material, $plant)
     {
