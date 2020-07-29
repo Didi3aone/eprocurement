21b40ef1855aac0485c90bcea3cb3c8f6e074194
didi3aone <didithreeaone@gmail.com>
2020-07-29 17:46:05 +0700

    fixed dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -6,11 +6,17 @@ class HomeController
 {
     public function index()
     {
+        $userMapping = \App\Models\UserMap::where('user_id', \Auth::user()->user_id)->first();
+        $userMapping = explode(',', $userMapping->purchasing_group_code);
         $prTotal = \App\Models\PurchaseRequest::where('purchase_requests_details.qty','>',0)
                     ->join('purchase_requests_details','purchase_requests_details.request_id','=','purchase_requests.id')
+                    ->whereIn('purchase_requests_details.purchasing_group_code', $userMapping)
                     // ->groupBy('purchase_requests.id')
-                    ->distinct()
+                    // ->distinct()
                     ->count();
-        return view('home',\compact('prTotal'));
+
+        $poTotal = \App\Models\PurchaseOrder::where('created_by',\Auth::user()->user_id)->count();
+                    // dd($prTotal);
+        return view('home',\compact('prTotal','poTotal'));
     }
 }
