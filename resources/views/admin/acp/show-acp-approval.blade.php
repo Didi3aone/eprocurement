394822478d2e2edaaafc1a6b74dc460088866c80
didi3aone <didithreeaone@gmail.com>
2020-08-06 13:50:57 +0700

    fixed itugan

--- resources/views/admin/acp/show-acp-approval.blade.php
+++ resources/views/admin/acp/show-acp-approval.blade.php
@@ -105,7 +105,7 @@
                                 </tr>
                                 @endforeach
                                 <tr>
-                                    <td colspan={{ $rowSpan + 2 }}></td>
+                                    <td colspan=7></td>
                                     <td colspan={{ $rowSpan + $rowSpan }}>
                                         <b style="color:black;font-size:17px;">{{ \toDecimal($totalPrice) }}</b>
                                     </td>
