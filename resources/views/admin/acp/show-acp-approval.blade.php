a03e2aa99c4cd1c0a2eb02ec4303f8907ca3f7fe
didi3aone <didithreeaone@gmail.com>
2020-08-10 21:38:37 +0700

    FIX SHOW APPROVAL

--- resources/views/admin/acp/show-acp-approval.blade.php
+++ resources/views/admin/acp/show-acp-approval.blade.php
@@ -72,8 +72,10 @@
                                     <th style="text-align:center;">Description</th>
                                     <th style="text-align:center;">Unit</th>
                                     <th style="text-align:center;">Per</th>
+                                    <th style="text-align:center;">Qty Pr</th>
                                     <th style="text-align:center;">Currency</th>
                                     <th style="text-align:center;">Price</th>
+                                    <th style="text-align:center;">Total Price</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -92,8 +94,9 @@
                                     <td rowspan={{ $rowSpan }}>{!! $winner !!}</td>
                                     @foreach (\App\Models\AcpTableMaterial::getMaterialVendor($rows->vendor_code, $rows->master_acp_id) as $key => $row)
                                         @php
-                                            $total = (\removeComma($row->price) * $row->qty);
-                                            $totalPrice += ($total);
+                                            //$total = (\removeComma($row->price) * $row->qty);
+                                           // $totalPrice += ($total);
+                                            $totalPrice += ($row->total_price);
                                             $data = count(\App\Models\AcpTableMaterial::getMaterialVendor($rows->vendor_code, $rows->master_acp_id));
                                            // dd($data);
                                         @endphp
@@ -101,12 +104,14 @@
                                         <td>{{ \App\Models\MasterMaterial::getMaterialName($row->material_id)->description ?? $row->material_id  }}</td>
                                         <td>{{ $row->uom_code }}</td>
                                         <td>{{ $row->qty }}</td>
+                                        <td>{{ toDecimal($row->qty_pr) }}</td>
                                         <td>{{ $row->currency }}</td>
                                         <td style="text-align:right;">{{ \toDecimal($row->price) }}</td>
+                                        <td style="text-align:right;">{{ \toDecimal($row->total_price) }}</td>
                                 </tr>
                                 @endforeach
                                 <tr>
-                                    <td colspan=7></td>
+                                    <td colspan=9></td>
                                     <td colspan={{ $rowSpan + $rowSpan }} style="text-align:right;"> 
                                         <b style="color:black;font-size:17px;">{{ \toDecimal($totalPrice) }}</b>
                                     </td>
