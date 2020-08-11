322896de54243ebbd581a256caf6ffc56f3d1acb
didi3aone <didithreeaone@gmail.com>
2020-08-11 17:34:34 +0700

    fix uom code

--- resources/views/admin/purchase-order/show.blade.php
+++ resources/views/admin/purchase-order/show.blade.php
@@ -63,7 +63,7 @@
                         @foreach($purchaseOrder->orderDetail as $key => $value)
                         <tr>
                             <td>{{ $value->material_id." - ".$value->short_text }}</td>
-                            <td>{{ \App\Models\UomConvert::where('uom_1', $value->unit)->first()->uom_2 }}</td>
+                            <td>{{ \getUomCode($value->unit) }}</td>
                             <td>{{ $value->qty }}</td>
                             <td>{{ $value->price }}</td>
                         </tr>
