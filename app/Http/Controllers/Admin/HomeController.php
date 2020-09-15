18de9ab9e47cf4ee1c22d957277d1b6d3192577f
Rifky Fahrizal <rifkyfahrizal22@gmail.com>
2020-09-15 14:35:40 +0700

    fix count purchase request & purcghase order di dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -6,21 +6,138 @@ class HomeController
 {
     public function index()
     {
-        //dd(\Auth::user()->roles[0]->id);
         $userMapping = \App\Models\UserMap::where('user_id', \Auth::user()->user_id)->first();
         $userMapping = explode(',', $userMapping->purchasing_group_code);
-        $prTotal = \App\Models\PurchaseRequest::where('purchase_requests_details.qty','>',0)
+
+        //PR TOTAL
+        $prTotalBefore = \App\Models\PurchaseRequest::where('purchase_requests_details.qty','>',0)
                     ->join('purchase_requests_details','purchase_requests_details.request_id','=','purchase_requests.id')
                     ->whereIn('purchase_requests_details.purchasing_group_code', $userMapping)
                     // ->groupBy('purchase_requests.id')
                     // ->distinct()
                     ->count();
+                    
+        $queryPr = \App\Models\PurchaseRequestsDetail::select(
+            \DB::raw('purchase_requests_details.id as id'),
+            'purchase_requests_details.request_id',
+            'purchase_requests_details.description',
+            'purchase_requests_details.qty',
+            'purchase_requests_details.unit',
+            'purchase_requests_details.notes',
+            'purchase_requests_details.price',
+            'purchase_requests_details.preq_item',
+            'purchase_requests_details.preq_name',
+            'purchase_requests_details.plant_code',
+            'purchase_requests_details.short_text',
+            'purchase_requests_details.storage_location',
+            'purchase_requests_details.material_group',
+            'purchase_requests_details.purchasing_group_code',
+            'purchase_requests_details.material_id',
+            'purchase_requests_details.delivery_date',
+            \DB::raw('purchase_requests_details.request_no as rn_no'),
+            'purchase_requests_details.release_date',
+            \DB::raw('purchase_requests.request_no as pr_no'),
+            'purchase_requests.request_date',
+            'purchase_requests.PR_NO',
+            'purchase_requests.total',
+            'purchase_requests.doc_type',
+            'purchase_requests.id as uuid'
+        )
+            ->join('purchase_requests', 'purchase_requests.id', '=', 'purchase_requests_details.request_id')
+            ->whereNotNull('purchase_requests.PR_NO')
+            ->where('purchase_requests_details.qty', '>', 0)
+            ->where('purchase_requests_details.is_validate', \App\Models\PurchaseRequestsDetail::YesValidate)
+            ->whereIn('purchase_requests_details.purchasing_group_code', $userMapping)
+            ->whereIn('purchase_requests_details.status_approval', [\App\Models\PurchaseRequestsDetail::Approved, \App\Models\PurchaseRequestsDetail::ApprovedPurchasing])
+            // ->where('purchase_requests_details.line_no', '0000000001')
+            //     ->orWhere('purchase_requests_details.line_no', '000000000')
+            // ->where(function ($query) {
+            //     $query->where('purchase_requests_details.status_approval', PurchaseRequestsDetail::Approved)
+            //         ->orWhere('purchase_requests_details.status_approval', PurchaseRequestsDetail::ApprovedPurchasing);
+            // })
+            ->whereIn('purchase_requests.status_approval', [\App\Models\PurchaseRequest::ApprovedDept, \App\Models\PurchaseRequest::ApprovedProc])
+            ->get();
+
+        $prTotal = count($queryPr);
+
+
+        //PO TOTAL
         if( \Auth::user()->roles[0]->id == 1 ) {
-            $poTotal = \App\Models\PurchaseOrder::count();
+            $queryPo = \App\Models\PurchaseOrdersDetail::join('purchase_orders', 'purchase_orders.id', '=', 'purchase_orders_details.purchase_order_id')
+                ->leftJoin('master_acps', 'master_acps.id', '=', 'purchase_orders_details.acp_id')
+                ->leftJoin('vendors', 'vendors.code', '=', 'purchase_orders.vendor_id')
+                // ->join('quotation','quotation.id','=','purchase_orders.quotation_id')
+                ->where('purchase_orders.status_approval', \App\Models\PurchaseOrder::Approved)
+                ->where('is_active', \App\Models\PurchaseOrdersDetail::Active)
+                // ->where('quotation.status',PurchaseOrder::POrepeat)
+                ->select(
+                    'purchase_orders_details.purchasing_document',
+                    'purchase_orders_details.PO_ITEM',
+                    'purchase_orders_details.material_id',
+                    'purchase_orders_details.short_text',
+                    'purchase_orders_details.storage_location',
+                    'purchase_orders_details.qty',
+                    'purchase_orders_details.qty_gr',
+                    'purchase_orders_details.qty_billing',
+                    'purchase_orders_details.unit',
+                    'purchase_orders_details.currency as original_currency',
+                    'purchase_orders_details.original_price',
+                    'purchase_orders.currency',
+                    'purchase_orders_details.price',
+                    'purchase_orders_details.tax_code',
+                    'purchase_orders_details.id as detail_id',
+                    'purchase_orders_details.request_no',
+                    'purchase_orders_details.plant_code',
+                    'purchase_orders_details.purchasing_group_code',
+                    'purchase_orders.po_date',
+                    'purchase_orders.PO_NUMBER',
+                    'purchase_orders.id',
+                    'purchase_orders.vendor_id',
+                    'master_acps.acp_no',
+                    'vendors.name as vendor'
+                )->get();
+            $poTotal = count($queryPo);
+            //$poTotal = \App\Models\PurchaseOrder::count();
         } else {
-            $poTotal = \App\Models\PurchaseOrder::where('created_by',\Auth::user()->user_id)->count();
+            $queryPo = \App\Models\PurchaseOrdersDetail::join('purchase_orders', 'purchase_orders.id', '=', 'purchase_orders_details.purchase_order_id')
+                ->leftJoin('master_acps', 'master_acps.id', '=', 'purchase_orders_details.acp_id')
+                ->leftJoin('vendors', 'vendors.code', '=', 'purchase_orders.vendor_id')
+                // ->join('quotation','quotation.id','=','purchase_orders.quotation_id')
+                ->where('purchase_orders.status_approval', \App\Models\PurchaseOrder::Approved)
+                ->where('is_active', \App\Models\PurchaseOrdersDetail::Active)
+                ->where('purchase_orders.created_by',\Auth::user()->user_id)
+                // ->where('quotation.status',PurchaseOrder::POrepeat)
+                ->select(
+                    'purchase_orders_details.purchasing_document',
+                    'purchase_orders_details.PO_ITEM',
+                    'purchase_orders_details.material_id',
+                    'purchase_orders_details.short_text',
+                    'purchase_orders_details.storage_location',
+                    'purchase_orders_details.qty',
+                    'purchase_orders_details.qty_gr',
+                    'purchase_orders_details.qty_billing',
+                    'purchase_orders_details.unit',
+                    'purchase_orders_details.currency as original_currency',
+                    'purchase_orders_details.original_price',
+                    'purchase_orders.currency',
+                    'purchase_orders_details.price',
+                    'purchase_orders_details.tax_code',
+                    'purchase_orders_details.id as detail_id',
+                    'purchase_orders_details.request_no',
+                    'purchase_orders_details.plant_code',
+                    'purchase_orders_details.purchasing_group_code',
+                    'purchase_orders.po_date',
+                    'purchase_orders.PO_NUMBER',
+                    'purchase_orders.id',
+                    'purchase_orders.vendor_id',
+                    'master_acps.acp_no',
+                    'vendors.name as vendor'
+                )->get();
+            $poTotal = count($queryPo);
+            //$poTotal = \App\Models\PurchaseOrder::where('created_by',\Auth::user()->user_id)->count();
         }
 
+        //ACP TOTAL
         if( \Auth::user()->roles[0]->id == 1 ) {
             //$acpTotal = \App\Models\AcpTable::count();
             $query = \App\Models\AcpTable::select(
